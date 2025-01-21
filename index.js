52f258d3bfba185e92ff50a1da37b0dd4d6650dc
yafizh <a.chairil.yafizh@gmail.com>
2025-01-21 21:23:35 +0700

    install whatsapp-web.js and integrating with socket.io

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const { Client } = require('whatsapp-web.js');
+const QRCode = require('qrcode');
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+const client = new Client();
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
@@ -13,7 +16,13 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    client.on('qr', async (qr) => {
+        io.emit('qr-code', await QRCode.toDataURL(qr));
+    });
 });
+
+client.initialize();
 
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
